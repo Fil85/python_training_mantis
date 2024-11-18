dbea5174c31342d26c14cb93a2e969850aff9468
KLMN\Filimonov <filimonov@ascon.ru>
2024-11-18 10:51:28 +0300

    Тест на создание нового проекта (дальнейшая реализация).

--- test/test_add_project.py
+++ test/test_add_project.py
@@ -2,10 +2,10 @@ from model.project import Project
 
 
 def test_add_project(app):
-    project = Project(name="111", description="test")
+    project_new = Project(name="111", description="test")
     app.session.login("administrator", "root")
     #old_projects = app.get_project_list()
-    app.projects.create(project)
+    app.project.create(project_new)
     #new_projects = app.get_project_list()
     #old_projects.append(project)
     #assert sorted(old_projects, key=Project.id_or_max) == sorted(new_projects, key=Project.id_or_max)
