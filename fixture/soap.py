8f158eb604cc8c9582251886b34e2bc3d793e492
KLMN\Filimonov <filimonov@ascon.ru>
2024-11-25 09:05:41 +0300

    Незначительные исправления.

--- fixture/soap.py
+++ fixture/soap.py
@@ -8,14 +8,14 @@ class SoapHepler:
         self.app = app
 
     def can_login(self, username, password):
-        client = Client("http://localhost/mantisbt-1.2.20/api/soap/mantisconnect.php?wsdl")
+        client = Client(self.app.base_url + "/api/soap/mantisconnect.php?wsdl")
         try:
             return client.service.mc_login(username, password)
         except WebFault:
             return False
 
     def get_project_list_soap(self, username, password):
-        client = Client("http://localhost/mantisbt-1.2.20/api/soap/mantisconnect.php?wsdl")
+        client = Client(self.app.base_url + "/api/soap/mantisconnect.php?wsdl")
         try:
             return client.service.mc_projects_get_user_accessible(username, password)
         except WebFault:
